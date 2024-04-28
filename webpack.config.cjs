18a61f5bf10aa69aa8f289f31f612c36274d1b45
Cruz Bonilla Alejandro <alex.139139.cb@gmail.com>
2024-04-28 17:17:47 -0600

    test webpack types

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,6 +1,8 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
+const glob = require('glob');
+const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TypeScriptDeclarationPlugin = require('typescript-declaration-webpack-plugin');
 
@@ -8,59 +10,88 @@ const isProduction = process.env.NODE_ENV == "production";
 
 const stylesHandler = MiniCssExtractPlugin.loader;
 
+const componentsEntries = glob.sync('./src/components/**/*.ts').reduce((entries, entry) => {
+	const namePath = path.basename(path.dirname(entry));
+	const componentName = `components/${namePath}`;
+	entries[componentName] = `./${entry}`;
+	return entries;
+}, {})
+
+
 const config = {
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist")
-  },
-  devServer: {
-    open: true,
-    host: "localhost"
-  },
-  plugins: [
-    new MiniCssExtractPlugin(),
-		new TypeScriptDeclarationPlugin({
-      out: './dist/types/index.d.ts',
-      removeComments: true, // Opcional: para eliminar comentarios de las definiciones
-    }),
+	// entry: "./src/index.ts",
 
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"]
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"]
-      },
-      {
-        test: /\.css$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader"]
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset"
-      }
+	entry: {
+		// index: './src/index.ts',
+		['components/Button']: './src/components/Button/index.ts',
+		// ...componentsEntries
+	},
 
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
-    ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", "..."]
-  }
+	// output: {
+	//   path: path.resolve(__dirname, "dist")
+	// },
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+		// library: 'React Base UI',
+		// libraryTarget: 'umd',
+	},
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				extractComments: false,
+			})
+		],
+	},
+
+	devServer: {
+		open: true,
+		host: "localhost"
+	},
+	plugins: [
+		new MiniCssExtractPlugin(),
+		// new TypeScriptDeclarationPlugin({
+		// 	out: "index.d.ts",
+		// }),
+
+		// Add your plugins here
+		// Learn more about plugins from https://webpack.js.org/configuration/plugins/
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx)$/i,
+				loader: "ts-loader",
+				exclude: ["/node_modules/"]
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"]
+			},
+			{
+				test: /\.css$/i,
+				use: [stylesHandler, "css-loader", "postcss-loader"]
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+				type: "asset"
+			}
+
+			// Add your rules for custom modules here
+			// Learn more about loaders from https://webpack.js.org/loaders/
+		]
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".jsx", ".js", "..."]
+	}
 };
 
 module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-  } else {
-    config.mode = "development";
-  }
-  return config;
+	if (isProduction) {
+		config.mode = "production";
+	} else {
+		config.mode = "development";
+	}
+	return config;
 };
