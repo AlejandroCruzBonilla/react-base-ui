43cd8236b6845bb54300ca29b7d74fd5bbf937f6
Cruz Bonilla Alejandro <alex.139139.cb@gmail.com>
2024-04-29 00:41:53 -0600

    add basic styles Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,49 +1,42 @@
-import React from 'react'
+import React from 'react';
 import classnames from 'classnames';
 import { useComponentByPass } from '../../hooks/useComponentByPass';
 import type { ButtonProps } from './interfaces';
 
-export const Button: React.FC<ButtonProps> = (props) => {
+export const Button: React.FC<ButtonProps> = props => {
+  const {
+    ariaLabel,
+    byPass,
+    children,
+    disabled = false,
+    label,
+    onClick,
+    type = 'button',
+  } = props;
 
-	const { 
-		ariaLabel,
-		byPass,
-		children,
-		disabled = false,
-		label,
-		onClick,
-		type = 'button',
-	} = props
-
-	const {classNames, styles} = useComponentByPass<ButtonProps>(props,byPass)
+  const { classNames, styles } = useComponentByPass<ButtonProps>(props, byPass);
 
   return (
-		<button
-			data-component-section='root'
-			aria-label={
-				ariaLabel
-				? ariaLabel
-				: label ? label : undefined
-			}
-			type={type}
-			onClick={onClick}
-			className={
-				classnames('base-ui__button',classNames['root'])
-			}
-			style={styles['root']}
-			disabled={disabled}
-		>
-			<span 
-				data-component-section='label'
-				className={
-					classnames('base-ui__button-label', classNames['label'])
-				}
-				style={styles['label']}
-			>
-				{label}
-			</span>
-			{children}
-		</button>
-	);
-};
+    <button
+      data-component-section='root'
+      aria-label={ariaLabel ? ariaLabel : label ? label : undefined}
+      type={type}
+      onClick={onClick}
+      className={classnames('base-ui__button', classNames['root'])}
+      style={styles['root']}
+      disabled={disabled}
+    >
+      {label && (
+        <span
+          data-component-section='label'
+          className={classnames('base-ui__button-label', classNames['label'])}
+          style={styles['label']}
+        >
+          {label}
+        </span>
+      )}
 
+      {children}
+    </button>
+  );
+};
