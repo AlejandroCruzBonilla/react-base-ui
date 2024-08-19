be81d82ef864d23d6530f3190fc7abb4c3e88fc8
Alex139139 <alex.139139.cb@gmail.com>
2024-08-19 14:56:09 -0600

    chore: Add shadcn dependencies

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import classnames from 'classnames';
 import { useComponentByPass } from '../../../hooks/useComponentByPass';
 import type { ButtonProps } from './interfaces';
 
@@ -38,20 +37,12 @@ export const Button: React.FC<ButtonProps> = (props) => {
 			}
 			type={type}
 			onClick={onClick}
-			className={classnames(
-				'base-ui__button',
-				severityVariantClass,
-				size ? `base-ui__button--${size}` : '',
-				classNames['root'],
-				{ 'base-ui--rounded': rounded },
-			)}
 			style={styles['root']}
 			disabled={disabled}
 		>
 			{icon && (
 				<span
 					data-component-section="icon"
-					className={classnames('base-ui__button-icon', classNames['icon'])}
 					style={styles['icon']}
 				>
 					{typeof icon === 'string' ?
@@ -62,7 +53,6 @@ export const Button: React.FC<ButtonProps> = (props) => {
 			{label && (
 				<span
 					data-component-section="label"
-					className={classnames('base-ui__button-label', classNames['label'])}
 					style={styles['label']}
 				>
 					{label}
