e531435505965d1218041ba8cb62dcbba432dbf3
Alex139139 <alex.139139.cb@gmail.com>
2024-09-09 10:03:14 -0600

    chore: Remove comments

--- src/components/ui/Avatar/AvatarImage.tsx
+++ src/components/ui/Avatar/AvatarImage.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as AvatarPrimitive from '@radix-ui/react-avatar';
-// import type { VariantProps } from 'class-variance-authority';
+
 import { cn } from '@/lib/utils';
 import { AvatarContext } from './AvatarContext';
 
